48f9a66ec3d992edcab01a63169d9778810a98f1
EQMVII <coelomate@gmail.com>
2016-12-17 18:23:10 -0500

    barely working draft API

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,30 +1,120 @@
 'use strict';
+var bodyParser = require('body-parser');
+var ObjectID = require('mongodb').ObjectID;
 
 var test = {hello: "world"};
 console.log(test);
 
-module.exports = function (app) {
+// routes for API endpoints
+module.exports = function (app, db) {
+  var jsonParser = bodyParser.json();
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.route('/')
     .get(function(req,res) {
-      res.json("hello");
+      res.sendFile(process.cwd() + '/public/index.html');
     });
 
-  app.route('/api/posts')
+  app.route('/api/posts/')
     .get(function(req,res) {
-      res.json(test);
+      // get a list from the db and return it
+      findAllDocuments(db, function(docs) {
+        //res.send([{test:"test"},{testTwo:"testTwo"},req.params.key, req.params.id]);
+        res.send(docs);
+        //db.close();
+        });
+
     })
-    .post(function(req,res) {
-      res.json("new post");
+    .post(jsonParser, function(req,res) {
+      if (!req.body) return res.sendStatus(400)
+      // add a post to the db
+      var newPost = JSON.parse(req.body.blogpost);
+      //res.json(req.body.id);
+      console.log(newPost);
+      insertDocuments(db, newPost, function() {
+        res.send("Blog post received");
+        //db.close();
+        });
+
     });
 
   app.route('/api/posts/:id')
     .get(function(req,res) {
-      res.json("Here is post #" + req.params.id);
+      // return a single post from the db
+      var docNumber = req.params.id;
+      //res.json("Here is post #" + req.params.id);
+      findADocument(db, docNumber, function(docs) {
+        //res.send(docs);
+        //console.log(docs);
+        res.send(docs);
+        //db.close();
+      });
     });
 
  // Not yet working via postman; responds that it cannot delete?
   app.route('/api/posts/:id')
     .delete(function(req,res) {
-      res.send("Delete request to endpoint for post #" + req.params.id);
+      // delete a post from the db
+      var docNumber = req.params.id;
+      removeDocument(db, docNumber, function() {
+        res.send("Document deleted");
+        //db.close();
+      });
+      //res.send("Delete request to endpoint for post #" + req.params.id);
     });
+}
+
+var insertDocuments = function(db, newPost, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertOne(newPost, function(err, result) {
+    //assert.equal(err, null);
+    //assert.equal(3, result.result.n);
+    //assert.equal(3, result.ops.length);
+    console.log("Inserted the new post");
+    callback(result);
+  });
+}
+
+var removeDocument = function(db, postNumber, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.deleteOne({ _id: ObjectID(postNumber) }, function(err, result) {
+    //assert.equal(err, null);
+    //assert.equal(1, result.result.n);
+    console.log("Removed the document with the ID of " + postNumber);
+    callback(result);
+  });
+}
+
+// Find one doc (right now where key a has the property value of 3)
+var findADocument = function(db, postNumber, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({ _id: ObjectID(postNumber) }, function(err, docs) {
+    //assert.equal(err, null);
+    var arraified = docs.toArray(function(err,docz){
+      console.log(docz[0]);
+      callback(docz[0]);
+    })
+    //console.log("Found the requested blog post without error");
+    //console.log(docs);
+
+  });
+}
+
+// Find a maximum of 40 of the docs as an array
+var findAllDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).limit(40).toArray(function(err, docs) {
+    //assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs);
+    //res.send(docs);
+    callback(docs);
+  });
 }
--- server.js
+++ server.js
@@ -2,17 +2,32 @@
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+var bodyParser = require('body-parser');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
 
 var app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server at " + url);
 
-routes(app);
+  app.use('/public', express.static(process.cwd() + '/public'));
+  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-// top line will allow the port to be set by heroku or default to 8080 for local
-// testing of the API server
-var port = process.env.PORT || 8080;
-app.listen(port, function () {
-  console.log('Node.js listening on port ' + port + '...');
-})
+  routes(app, db);
+
+  // top line will allow the port to be set by heroku or default to 8080 for local
+  // testing of the API server
+  var port = process.env.PORT || 8080;
+  app.listen(port, function () {
+    console.log('Node.js listening on port ' + port + '...');
+  })
+
+  //db.close();
+
+});
